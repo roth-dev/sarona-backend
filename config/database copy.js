04f7ab01c673703d9e01e21de2bb69d2b489058c
roth-dev <roth.dev.ops@gmail.com>
2021-07-06 22:32:52 +0700

    :boo: add cart collection type

---
+++ config/database copy.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
